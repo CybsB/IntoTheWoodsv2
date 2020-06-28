4165df65a331fc15a5e18d029a955f8af85c1ce7
CybsB <37172568+CybsB@users.noreply.github.com>
2020-06-28 20:44:26 +0100

    Adding Web facing pages

---
+++ intothewoodsv2/firebase.js
@@ -0,0 +1,10 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyC5vdbYicbp_K6GfaaY2ZLKxa3LzPkhX8o",
+    authDomain: "cloud-camera-e206c.firebaseapp.com",
+    databaseURL: "https://cloud-camera-e206c.firebaseio.com",
+    projectId: "cloud-camera-e206c",
+    storageBucket: "cloud-camera-e206c.appspot.com",
+    messagingSenderId: "74972719029",
+    appId: "1:74972719029:web:0dcfaa7ac0dad670"
+  };
+  firebase.initializeApp(firebaseConfig);
---
+++ intothewoodsv2/firebaseperf.js
@@ -0,0 +1,2 @@
+//allows for firebas perfomance to be initialized. MUST BE USED IN SN HTML FILE WITH 'firebse.js' and the firebase performance SDK
+var perf = firebase.performance();
